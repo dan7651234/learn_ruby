b8240c490e9d1ab2cc3a7ec037729e039537d65e
Dan Bilunas <Dan7651234@gmail.com>
2017-07-09 12:21:24 -0700

    Simple temp calculator to convert temps from c to f and f to c. All tests passed.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,12 @@
 #write your code here
+def ftoc fTemp
+  fTemp = fTemp.to_f
+  cTemp = ((fTemp - 32) * 5) / 9
+  return cTemp
+end
+
+def ctof cTemp
+  cTemp = cTemp.to_f
+  fTemp = cTemp * 9 / 5 + 32
+  return fTemp
+end
