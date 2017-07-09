0fdf88fbcae7df03fb384b11bee4fada4fef0f00
Dan Bilunas <Dan7651234@gmail.com>
2017-07-09 12:19:47 -0700

    Calculator section of project contains add, subtract, sum, multiply, power, and factorial functions. Tests written for multiply, power, and factorial.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,41 @@
 #write your code here
+def add num1, num2
+  result = num1 + num2
+  return result
+end
+
+def subtract num1, num2
+  result = num1 - num2
+  return result
+end
+
+def sum nums
+  result = 0
+  nums.each {|x| result = result + x}
+  return result
+end
+
+def multiply *nums
+  result = 1
+  nums.each {|x| result = result * x}
+  return result
+end
+
+def power num1, num2
+  result = num1**num2
+  return result
+end
+
+def factorial num
+  num = num.to_i
+  if num == 0 || num == 1
+    return 1
+  else
+    i = num - 1
+    while i > 1
+      num = num * i
+      i = i - 1
+    end
+  end
+  return num
+end
