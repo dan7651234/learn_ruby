ad4a443ae1a055391d9a32eb6678cf109a970e92
Dan Bilunas <Dan7651234@gmail.com>
2017-07-15 14:05:43 -0700

    Adding Book Title exercise. Took less time than expected. All tests passed.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,27 @@
 class Book
 # write your code here
+  def initialize
+    @title = ""
+  end
+
+  def title= newTitle
+    @title = newTitle.to_s
+  end
+
+  def title
+    titleSplitter = @title.split
+    @title = ""
+    littleWords = ["and", "or","is","in","of","the","a","an"]
+    firstWord = true
+    titleSplitter.each do |i|
+      if !littleWords.include?(i) || firstWord == true
+        @title = @title + i.capitalize + " "
+        firstWord = false
+      else
+        @title = @title + i.downcase + " "
+      end
+    end
+    @title = @title.strip
+    return @title
+  end
 end
